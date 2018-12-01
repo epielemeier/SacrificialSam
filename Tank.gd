64a070f8323e1cc0eecd31c18945d64eddcd9855
emilypie <empielemeier@gmail.com>
2018-12-01 10:46:22 -0600

    Removing rotation tween

--- Tank.gd
+++ Tank.gd
@@ -18,20 +18,6 @@ func shoot(pos):
 
 func rotate_head(new_rotation):
 	$Sprite/HeadSprite.rotation = new_rotation
-	#var rot_speed = 2*PI #rotation speed in degrees per second
-	#var angle_diff = abs($Sprite/HeadSprite.rotation - new_rotation)
-	#if 2*PI - $Sprite/HeadSprite.rotation > PI && 2*PI - new_rotation < PI:
-	#	new_rotation += 2*PI
-	#if angle_diff != 0:
-	#	var time_to_turn = abs(new_rotation / rot_speed)
-	#	
-	#	$RotationTween.interpolate_property($Sprite/HeadSprite,"rotation",
-	#		$Sprite/HeadSprite.rotation,
-	#		new_rotation,
-	#		time_to_turn,
-	#		Tween.TRANS_QUAD,
-	#		Tween.EASE_OUT_IN)
-	#	$RotationTween.start()
 
 
 func _on_Visibility_body_entered(body):
@@ -43,4 +29,3 @@ func _on_Visibility_body_entered(body):
 func _on_Visibility_body_exited(body):
 	if body == target:
 		target = null
-		#$RotationTween.stop($Sprite/HeadSprite)
